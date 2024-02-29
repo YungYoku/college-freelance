8c75d66318fba92301fdc5b012de54903cff859c
Maxim <yungyoku@yandex.ru>
2024-02-29 22:14:46 +0300

    Added field into offer c/e page

--- src/views/OfferView.vue
+++ src/views/OfferView.vue
@@ -1,22 +1,58 @@
 <template>
-    Деталка оффера
+    <div class="offer">
+        <div class="offer__title">
+            {{ offer.title }}
+        </div>
+
+        <div class="offer__description">
+            {{ offer.description }}
+        </div>
+
+        <div class="offer__price">
+            {{ offer.price }}
+        </div>
+
+        <div class="offer__rating">
+            {{ offer.rating }}
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import { useRoute } from 'vue-router'
+
 import http from '@/plugins/http'
+import { JobOffer } from '@/interfaces/JobOffer.ts'
 
 const route = useRoute()
 
 const { id } = route.params
 
+const offer = ref<JobOffer>({
+    collectionId: '',
+    collectionName: '',
+    created: '',
+    creator: '',
+    deadline: new Date(),
+    description: '',
+    discipline: '',
+    id: '',
+    price: 0,
+    rating: 0,
+    status: 0,
+    title: '',
+    university: '',
+    updated: ''
+})
+
 const loadOffer = async () => {
     if (!id) return
 
     await http
-        .get(`/collections/job_offers/records/${id}`)
+        .get<JobOffer>(`/collections/job_offers/records/${id}`)
         .then(response => {
-            console.log(response)
+            offer.value = response
         })
 }
 loadOffer()
