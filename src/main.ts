3acd51e44a0c0672ab90d9f1f99950e624e6f75a
Maxim <yungyoku@yandex.ru>
2024-04-27 11:21:29 +0300

    Updated dependencies

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import './assets/index.css'
-import 'swiper/css'
-import 'swiper/css/pagination'
-import 'swiper/css/navigation'
 
 const app = createApp(App)
 
