e516e58225b148bb20ca9dec5cfe5a0a0c6eb697
Maxim <yungyoku@yandex.ru>
2024-04-28 15:40:19 +0300

    v0.0.7

--- src/components/blocks/JobSearch.vue
+++ src/components/blocks/JobSearch.vue
@@ -8,19 +8,31 @@
 			placeholder="Поиск"
 		/>
 
-		<Button v-if="value">
-			Поиск
-		</Button>
+		<template v-if="value">
+			<Skeleton
+				v-if="searchStore.loading"
+				class="h-9"
+			/>
+
+			<Button
+				v-else
+				@click="search"
+			>
+				Поиск
+			</Button>
+		</template>
 	</Grid>
 </template>
 
 <script setup lang="ts">
 import { ref, computed, watch } from 'vue'
+import { useRouter } from 'vue-router'
 import { useSearchStore } from '@/stores/search.ts'
 
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import Grid from '@/components/structures/Grid.vue'
+import { Skeleton } from '@/components/ui/skeleton'
 
 const value = ref('')
 
@@ -31,6 +43,12 @@ const columns = computed(() => {
 	if (value.value.length) return [4, 1]
 	return 1
 })
+
+const router = useRouter()
+const search = () => {
+	searchStore.update(value.value)
+	router.push('/search')
+}
 </script>
 
 <style scoped lang="scss">
