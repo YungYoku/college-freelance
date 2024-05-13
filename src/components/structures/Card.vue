79baf93d9ae6102866fcb41bc12d03825b270e29
Yoku <yungyoku@yandex.ru>
2024-05-13 17:33:19 +0300

    Refactoring

---
+++ src/components/structures/Card.vue
@@ -0,0 +1,30 @@
+<template>
+	<Card :class="`w-[${width}]`">
+		<CardHeader v-if="title">
+			<CardTitle>
+				{{ title }}
+			</CardTitle>
+		</CardHeader>
+
+		<CardContent>
+			<div class="grid items-center w-full gap-2">
+				<slot/>
+			</div>
+		</CardContent>
+	</Card>
+</template>
+
+<script setup lang="ts">
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+
+defineProps({
+	width: {
+		type: String,
+		default: '300px'
+	},
+	title: {
+		type: String,
+		default: null
+	}
+})
+</script>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,57 +1,50 @@
 <template>
 	<AuthLayout>
-		<Card class="w-[300px]">
-			<CardHeader>
-				<CardTitle>
-					Вход
-				</CardTitle>
-			</CardHeader>
+		<Card
+			width="300px"
+			title="Вход"
+		>
+			<Input
+				v-model.trim="form.identity"
+				placeholder="Логин или почта"
+				type="text"
+			/>
 
-			<CardContent>
-				<div class="grid items-center w-full gap-2">
-					<Input
-						v-model.trim="form.identity"
-						placeholder="Логин или почта"
-						type="text"
-					/>
+			<Input
+				v-model.trim="form.password"
+				placeholder="Пароль"
+				type="password"
+			/>
 
-					<Input
-						v-model.trim="form.password"
-						placeholder="Пароль"
-						type="password"
-					/>
+			<Skeleton
+				v-if="loading"
+				class="h-9"
+			/>
 
-					<Skeleton
-						v-if="loading"
-						class="h-9"
-					/>
+			<Button
+				v-else
+				type="submit"
+				@click="login"
+			>
+				Войти
+			</Button>
 
-					<Button
-						v-else
-						type="submit"
-						@click="login"
-					>
-						Войти
-					</Button>
+			<div class="mt-2 text-center text-sm">
+				Нет аккаунта?
+				<router-link
+					to="/registration"
+					class="underline"
+				>
+					Зарегистрироваться
+				</router-link>
+			</div>
 
-					<div class="mt-2 text-center text-sm">
-						Нет аккаунта?
-						<router-link
-							to="/registration"
-							class="underline"
-						>
-							Зарегистрироваться
-						</router-link>
-					</div>
-
-					<router-link
-						class="text-center text-sm underline"
-						to="/login"
-					>
-						Забыли пароль?
-					</router-link>
-				</div>
-			</CardContent>
+			<router-link
+				class="text-center text-sm underline"
+				to="/login"
+			>
+				Забыли пароль?
+			</router-link>
 		</Card>
 	</AuthLayout>
 </template>
@@ -68,7 +61,7 @@ import AuthLayout from '@/components/layouts/AuthLayout.vue'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { Skeleton } from '@/components/ui/skeleton'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import Card from '@/components/structures/Card.vue'
 import { useToast } from '@/components/ui/toast'
 
 const auth = useAuthStore()
