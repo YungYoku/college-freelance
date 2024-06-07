4542af1afa9ffa85526981b1d30fbb4b3edc687f
Nikita <samorodsky1010@mail.ru>
2024-06-07 15:06:20 +0300

    Added Text component, pageTitle changed

--- src/components/elements/pageTitle.vue
+++ src/components/elements/pageTitle.vue
@@ -1,55 +1,20 @@
 <template>
 	<Skeleton
 		v-if="loading"
-		class="w-[400px] max-w-[100%]"
-		:class="{
-			'h-12': size === 'l',
-			'h-10': size === 'm',
-			'h-8': size === 's',
-			'h-6': size === 'xs'
-		}"
+		class="w-[400px] max-w-[100%] h-12"
 	/>
 
-	<h1
-		v-else-if="size === 'l'"
-		class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl"
+	<Text
+		size="l"
 	>
 		<slot/>
-	</h1>
-
-	<h2
-		v-else-if="size === 'm'"
-		class="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0"
-	>
-		<slot/>
-	</h2>
-
-	<h3
-		v-else-if="size === 's'"
-		class="scroll-m-20 text-2xl font-semibold tracking-tight"
-	>
-		<slot/>
-	</h3>
-
-	<h4
-		v-else-if="size === 'xs'"
-		class="scroll-m-20 text-xl font-semibold tracking-tight"
-	>
-		<slot/>
-	</h4>
+	</Text>
 </template>
 
 <script setup lang="ts">
 import { Skeleton } from '@/components/ui/skeleton'
-
+import { Text } from '@/components/elements'
 defineProps({
-	size: {
-		type: String,
-		default: 'm',
-		validator: (val: string) => {
-			return ['xs', 's', 'm', 'l'].includes(val)
-		}
-	},
 	loading: {
 		type: Boolean,
 		default: false
