904bb762f8e467efc6157fa99eb57eb7e7734cf8
Yoku <yungyoku@yandex.ru>
2024-06-24 10:57:19 +0300

    Добавлена возможность искать по исполнителям (без учета фильтров).

--- src/views/search.vue
+++ src/views/search.vue
@@ -11,56 +11,95 @@
 			label="Поиск"
 		/>
 
+		<Select
+			v-model="form.entity.value"
+			:error="form.entity.error"
+			:items="entitiesItems"
+			label="Сущность"
+		/>
+
 		<SelectLive
-			v-model="university"
+			v-model="form.university.value"
+			:error="form.university.error"
 			place-holder="Университет"
 			api="universities"
 		/>
 
 		<SelectLive
-			v-model="offerType"
+			v-model="form.type.value"
+			:error="form.type.error"
 			place-holder="Тип работы"
 			api="offer_types"
 		/>
 
 		<SelectLive
-			v-model="offerDiscipline"
+			v-model="form.discipline.value"
+			:error="form.discipline.error"
 			place-holder="Дисциплина"
 			api="disciplines"
 		/>
 
 		<Button
 			:disabled="loading"
-			@click="loadOffers"
+			@click="loadData"
 		>
 			Поиск
 		</Button>
 	</Grid>
 
-	<Grid
-		v-if="offers.length || loading"
-		:columns-xl="4"
-		:columns-l="3"
-		:columns-m="2"
-		:columns-s="1"
-	>
-		<template v-if="loading">
-			<EmptyJobOffer
-				v-for="i in 8"
-				:key="i"
-			/>
-		</template>
-		<template v-else>
-			<JobOffer
-				v-for="offer in offers"
-				:key="offer.id"
-				:job-offer="offer"
-				:loading="loading"
-			/>
-		</template>
-	</Grid>
+	<template v-if="form.entity.value === 'offer'">
+		<Grid
+			v-if="offers.length || loading"
+			:columns-xl="4"
+			:columns-l="3"
+			:columns-m="2"
+			:columns-s="1"
+		>
+			<template v-if="loading">
+				<EmptyJobOffer
+					v-for="i in 8"
+					:key="i"
+				/>
+			</template>
+			<template v-else>
+				<JobOffer
+					v-for="offer in offers"
+					:key="offer.id"
+					:job-offer="offer"
+					:loading="loading"
+				/>
+			</template>
+		</Grid>
+		<span v-else>Нет доступных объявлений.</span>
+	</template>
 
-	<span v-else>Нет доступных объявлений.</span>
+	<template v-else>
+		<Grid
+			v-if="executors.length || loading"
+			:columns-xl="4"
+			:columns-l="3"
+			:columns-m="2"
+			:columns-s="1"
+		>
+			<template v-if="loading">
+				<UserCard
+					v-for="i in 8"
+					:key="i"
+					loading
+					link
+				/>
+			</template>
+			<template v-else>
+				<UserCard
+					v-for="executor in executors"
+					:key="executor.id"
+					:user="executor"
+					link
+				/>
+			</template>
+		</Grid>
+		<span v-else>Нет доступных исполнителей.</span>
+	</template>
 </template>
 
 <script lang="ts" setup>
@@ -69,11 +108,20 @@ import { useAuthStore } from '@/stores/auth.ts'
 import { useSearchStore } from '@/stores/search.ts'
 
 import { Grid } from '@/components/structures'
-import { EmptyJobOffer, JobOffer, SelectLive, Button, Input } from '@/components/blocks'
+import { EmptyJobOffer, JobOffer, SelectLive, Button, Input, Select, UserCard } from '@/components/blocks'
 import { IJobOffer, IJobOffers } from '@/interfaces/JobOffer.ts'
-import { Datetime, Http, Screen } from '@/plugins'
+import { Datetime, Form, Http, Screen } from '@/plugins'
+import { IUser, IUsers } from '@/interfaces/User.ts'
+
+interface SearchForm {
+	entity: 'offer' | 'executor',
+	university: string
+	type: string
+	discipline: string
+}
 
 const offers = ref<Array<IJobOffer>>([])
+const executors = ref<Array<IUser>>([])
 
 const authStore = useAuthStore()
 const searchStore = useSearchStore()
@@ -90,24 +138,28 @@ watch(() => searchStore.search, (value) => {
 	search.value = value
 }, { immediate: true })
 
-const university = ref('')
-const offerType = ref('')
-const offerDiscipline = ref('')
+const form = Form<SearchForm>({
+	entity: 'offer',
+	university: '',
+	type: '',
+	discipline: ''
+})
+const entitiesItems = [
+	{ value: 'offer', text: 'Объявление' },
+	{ value: 'executor', text: 'Исполнитель' }
+]
 
 const loading = ref(true)
 const loadOffers = async () => {
-	loading.value = true
-	searchStore.setLoading(true)
-
 	const filters = []
 	let filter = ''
 	let encodedFilter = ''
 
 	if (!authStore.isAdmin) filters.push(`status='created' && deadline>='${Datetime.get(new Date())}'`)
 	if (search.value) filters.push(`title~'${search.value}'`)
-	if (university.value) filters.push(`university='${university.value}'`)
-	if (offerType.value) filters.push(`type='${offerType.value}'`)
-	if (offerDiscipline.value) filters.push(`discipline='${offerDiscipline.value}'`)
+	if (form.university.value) filters.push(`university='${form.university.value}'`)
+	if (form.type.value) filters.push(`type='${form.type.value}'`)
+	if (form.discipline.value) filters.push(`discipline='${form.discipline.value}'`)
 	if (filters.length) {
 		filter = filters.reduce((acc, filter) => filter ? `${acc} && ${filter}` : acc, '')
 		filter = filter.slice(4)
@@ -124,9 +176,32 @@ const loadOffers = async () => {
 		.then(res => {
 			offers.value = res.items
 		})
+}
+
+const loadUsers = async () => {
+	await Http
+		.get<IUsers>('/collections/users/records', {
+			filter: '(role=\'executor\')',
+			expand: ['university', 'rating', 'disciplines'],
+			perPage: 12
+		})
+		.then(res => {
+			executors.value = res.items
+		})
+}
+
+const loadData = async () => {
+	loading.value = true
+	searchStore.setLoading(true)
+
+	if (form.entity.value === 'offer') {
+		await loadOffers()
+	} else {
+		await loadUsers()
+	}
 
 	loading.value = false
 	searchStore.setLoading(false)
 }
-loadOffers()
+loadData()
 </script>
