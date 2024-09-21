54205f5f239e7012f8969ca2353879584c84def7
Yoku <yungyoku@yandex.ru>
2024-09-21 13:31:04 +0300

    Updated dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,11 @@ export default defineConfig(({ mode }) => {
 			postcss: {
 				plugins: [tailwind(), autoprefixer()],
 			},
+			preprocessorOptions: {
+				scss: {
+					api: 'modern'
+				},
+			},
 		},
 
 		plugins: [
