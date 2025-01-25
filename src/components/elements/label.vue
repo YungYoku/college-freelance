b4c99d1712b783178a99e5934c55430c2c8d78e6
Yoku <yungyoku@yandex.ru>
2025-01-25 17:03:09 +0300

    Refactoring

--- src/components/elements/label.vue
+++ src/components/elements/label.vue
@@ -1,5 +1,17 @@
 <template>
-	<div class="absolute left-3 top-1 text-xs text-muted-foreground font-extralight">
+	<div class="label">
 		<slot/>
 	</div>
 </template>
+
+<style scoped lang="scss">
+.label {
+	position: absolute;
+	top: 4px;
+	left: 12px;
+
+	font-size: 12px;
+	font-weight: 200;
+	color: hsl(var(--muted-foreground));
+}
+</style>
--- src/components/structures/card.vue
+++ src/components/structures/card.vue
@@ -1,24 +1,27 @@
 <template>
 	<div
+		class="card"
 		:style="{
 			width
 		}"
-		class="max-w-[100%] border rounded-xl shadow"
 	>
 		<div
 			v-if="title"
-			class="font-semibold p-5 pt-4 pb-1"
+			class="card__title"
 		>
 			{{ title }}
 		</div>
 
-		<div class="grid items-center w-full gap-2 p-5">
+		<Grid
+			vertical
+			class="p-5"
+		>
 			<slot/>
-		</div>
+		</Grid>
 
 		<div
 			v-if="$slots.footer"
-			class="p-5"
+			class="card__footer"
 		>
 			<slot name="footer"/>
 		</div>
@@ -26,6 +29,8 @@
 </template>
 
 <script setup lang="ts">
+import { Grid } from '@/components/structures'
+
 defineProps({
 	width: {
 		type: String,
@@ -37,3 +42,25 @@ defineProps({
 	}
 })
 </script>
+
+<style lang="scss" scoped>
+.card {
+	max-width: 100%;
+
+	border: 1px solid hsl(var(--input));
+	border-radius: 12px;
+	--tw-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
+	--tw-shadow-colored: 0 1px 3px 0 var(--tw-shadow-color), 0 1px 2px -1px var(--tw-shadow-color);
+	box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
+
+	&__title {
+		padding: 16px 20px 4px 20px;
+
+		font-weight: 600;
+	}
+
+	&__footer {
+		padding: 20px;
+	}
+}
+</style>
